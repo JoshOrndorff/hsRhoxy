e5d37bbe57901a3049da10b5dd9743ea9d581819
Joshy Orndorff <admin@joshorndorff.com>
2019-01-29 22:21:48 -0500

    Wrote several tests for comment and filler parsing.

--- test/tests.hs
+++ test/tests.hs
@@ -6,10 +6,28 @@ import RhoParser
 
 -- TODO I can't get this file to give me results or failures with cabal test
 -- If I make it executible, it works as expected.
-tests = TestList [
+miscTests = TestList [
   TestCase (assertEqual "parsing Nil" (Right (Par [Nil])) (parseRhoc "Nil")),
   TestCase (assertEqual "parsing Send" (Right (Par [Send (Quote Nil) Nil])) (parseRhoc "@Nil!(Nil)")),
   TestCase (assertEqual "for (foo 3)," (1,2) (1,3))
   ]
 
-main = runTestTT tests
+
+commentTests = TestList [
+  TestCase (assertEqual "c1" (Right (Par [Nil])) (parseRhoc "Nil//c")),
+  TestCase (assertEqual "c2" (Right (Par [Nil])) (parseRhoc "Nil// c")),
+  TestCase (assertEqual "c3" (Right (Par [Nil])) (parseRhoc "Nil //comment")),
+  TestCase (assertEqual "c4" (Right (Par [Nil])) (parseRhoc "Nil // comment")),
+  TestCase (assertEqual "c5" (Right (Par [Nil])) (parseRhoc "Nil/* comment*/")),
+  TestCase (assertEqual "c6" (Right (Par [Nil])) (parseRhoc "Nil /* comment */")),
+  TestCase (assertEqual "c7" (Right (Par [Nil])) (parseRhoc "/* c */ Nil /* c */")),
+  TestCase (assertEqual "c8" (Right (Par [Nil])) (parseRhoc "/**/Nil")),
+  TestCase (assertEqual "c9" (Right (Par [Nil])) (parseRhoc "Nil/**/")),
+  TestCase (assertEqual "c10" (Right (Par [Nil])) (parseRhoc "Nil /* c */ //c")),
+  TestCase (assertEqual "c11" (Right (Par [Nil])) (parseRhoc "Nil//")),
+  TestCase (assertEqual "c12" (Right (Par [Nil])) (parseRhoc "/* c */ Nil //c")),
+  TestCase (assertEqual "c13" (Right (Par [Nil])) (parseRhoc "/**//**/Nil"))
+  ]
+
+
+main = runTestTT commentTests
